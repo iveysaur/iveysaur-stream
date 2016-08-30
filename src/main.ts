acab7feae84bd1a140243ed08b0ffb4c9aeb6977
ivey <iveysaurrr@gmail.com>
2016-08-29 21:49:43 -0400

    Add material to basic app with toolbar and menu

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,36 @@
-import { bootstrap } from '@angular/platform-browser-dynamic';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
 import { AppComponent, environment } from './app/';
+import { NgModule, ApplicationRef } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { MdButtonModule } from '@angular2-material/button';
+import { MdIconModule } from '@angular2-material/icon';
+import { MdMenuModule } from '@angular2-material/menu';
+import { MdToolbarModule } from '@angular2-material/toolbar';
+import { OverlayModule } from '@angular2-material/core/overlay/overlay-directives';
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    MdButtonModule,
+    MdIconModule,
+    MdMenuModule,
+    MdToolbarModule,
+    OverlayModule,
+  ],
+  declarations: [AppComponent],
+  entryComponents: [AppComponent]
+})
+export class AppModule {
+  constructor(private _appRef: ApplicationRef) { }
+
+  ngDoBootstrap() {
+    this._appRef.bootstrap(AppComponent);
+  }
+}
 
 if (environment.production) {
   enableProdMode();
 }
 
-bootstrap(AppComponent);
+platformBrowserDynamic().bootstrapModule(AppModule);
